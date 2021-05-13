54086a2b63357f4c7e0031e352dc96f23abea81e
Yuri Dias <yuri.dazevedo@gmail.com>
2021-05-13 17:27:59 -0300

    feat: Create login form

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,12 +4,17 @@ import {Image} from 'react-native';
 import {Container, Title} from './styles';
 
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/input';
+import Button from '../../components/button';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="senha" icon="lock" placeholder="Senha" />
+      <Button>Entrar</Button>
     </Container>
   );
 };
